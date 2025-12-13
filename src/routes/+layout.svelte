b0b9ccfb61753a64cc98735759b30969e8970331
Richard <richard@stromer.org>
2025-12-13 20:57:12 +0100

    remove unused theme store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
   import "../app.css";
-  import { themeStore } from "$lib/stores/theme";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    // Initialize theme on mount
-    $themeStore;
-  });
 </script>
 
 <slot />
