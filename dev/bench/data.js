bd1ccbf5aeec7ce18ec2085cfff03998f49f90a0
github-action-benchmark <github@users.noreply.github.com>
2025-12-13 19:46:46 +0000

    add Benchmark (cargo) benchmark result for 1ab34454cc761dd491b32bc61b29cb3424719f71

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765651960206,
+  "lastUpdate": 1765655206488,
   "repoUrl": "https://github.com/noxan/diskly",
   "entries": {
     "Benchmark": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
             "name": "scanner/deep_narrow_100_levels",
             "value": 2927205,
             "range": "± 40474",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "richard@stromer.org",
+            "name": "Richard",
+            "username": "noxan"
+          },
+          "committer": {
+            "email": "richard@stromer.org",
+            "name": "Richard",
+            "username": "noxan"
+          },
+          "distinct": true,
+          "id": "1ab34454cc761dd491b32bc61b29cb3424719f71",
+          "message": "remove theme toggle",
+          "timestamp": "2025-12-13T20:44:27+01:00",
+          "tree_id": "c635fdd628f7ecc46c86d1d9798a720be79cdfb6",
+          "url": "https://github.com/noxan/diskly/commit/1ab34454cc761dd491b32bc61b29cb3424719f71"
+        },
+        "date": 1765655205766,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "scanner/small_100_files_3_levels",
+            "value": 247784,
+            "range": "± 23127",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/medium_1000_files_5_levels",
+            "value": 1775380,
+            "range": "± 19275",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/large_10000_files_7_levels",
+            "value": 4896953,
+            "range": "± 76229",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/wide_flat_5000_files",
+            "value": 4033459,
+            "range": "± 52688",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "scanner/deep_narrow_100_levels",
+            "value": 1720451,
+            "range": "± 20669",
             "unit": "ns/iter"
           }
         ]
