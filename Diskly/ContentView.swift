1a514ccbc17e6ea78c69f58acd80e177ff5f2727
Richard <richard@stromer.org>
2026-06-28 11:29:30 +0200

    build: initialize xcode project for diskly macos app

---
+++ Diskly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Diskly
+//
+//  Created by Rick on 6/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Diskly/DisklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisklyApp.swift
+//  Diskly
+//
+//  Created by Rick on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct DisklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
